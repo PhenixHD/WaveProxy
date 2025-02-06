18d67c12451368ae712a813bd64273d56a3535c8
PhenixHD <eliasbiyikli09@gmail.com>
2025-02-06 08:11:37 +0100

    Updated Path & File Handling

--- WaveProxy/src/FileHandling/PathHandler.cs
+++ WaveProxy/src/FileHandling/PathHandler.cs
@@ -5,39 +5,63 @@
         Config
     }
 
+    public enum FileName {
+        appsettings,
+        scraped_proxies,
+        urls,
+        output_https,
+        output_socks4_5
+    }
+
     internal class PathHandler {
         public string InputFilePath { get; private set; }
         public string OutputFilePath { get; private set; }
         private string _currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
 
-        public PathHandler(DirectoryName directory, string inputFileName) {
-            InputFilePath = GetPath(directory, inputFileName);
+        public PathHandler(DirectoryName inputDirectory, FileName inputFileName) {
+            InputFilePath = GetPath(inputDirectory, inputFileName);
+            ValidateFileExists(InputFilePath);
         }
 
-        public PathHandler(DirectoryName directory, string inputFileName, string outputFileName) {
-            InputFilePath = GetPath(directory, inputFileName);
-            OutputFilePath = GetPath(directory, outputFileName);
+        public PathHandler(DirectoryName inputDirectory, FileName inputFileName, DirectoryName outputDirectory, FileName outputFileName) {
+            InputFilePath = GetPath(inputDirectory, inputFileName);
+            ValidateFileExists(InputFilePath);
+
+            OutputFilePath = GetPath(outputDirectory, outputFileName);
+            ValidateFileExists(OutputFilePath);
         }
 
-        public string GetPath(DirectoryName directory, string fileName) {
+        public string GetPath(DirectoryName directory, FileName filename) {
             string folderName = directory switch {
                 DirectoryName.Data => "data",
                 DirectoryName.Config => "config",
                 _ => throw new ArgumentException("Invalid directory name")
             };
 
-            string fullPath = Path.Combine(_currentDirectory, folderName, fileName);
+            string fileName = filename switch {
+                FileName.appsettings => "appsettings.json",
+                FileName.scraped_proxies => "proxies.txt",
+                FileName.urls => "urls.txt",
+                FileName.output_https => "https.txt",
+                FileName.output_socks4_5 => "socks4_5.txt",
+                _ => throw new ArgumentException("Invalid file name")
 
-            ValidatePath(fullPath);
+            };
 
-            return Path.Combine(_currentDirectory, folderName, fileName);
+            string directoryPath = Path.Combine(_currentDirectory, folderName);
+            ValidateDirectoryExists(directoryPath);
+
+            return Path.Combine(directoryPath, fileName);
         }
 
-        private void ValidatePath(string path) {
-            if (string.IsNullOrWhiteSpace(path))
-                throw new ArgumentException("Invalid Path input");
-            if (!Path.Exists(path))
-                throw new DirectoryNotFoundException("Path does not exist!");
+        private void ValidateDirectoryExists(string path) {
+            if (!Directory.Exists(path))
+                throw new DirectoryNotFoundException($"Directory not found: {path}");
+        }
+
+        private void ValidateFileExists(string path) {
+            if (!File.Exists(path))
+                throw new FileNotFoundException($"File not found: {path}");
         }
     }
 }
